91a462333ed0db19cc15bb092346597051a84716
zhq <32982289+zhq1@users.noreply.github.com>
2018-02-27 13:44:47 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,6 @@ dd if=/dev/zero of=/mnt/myswap.swap bs=1M count=4000
 mkswap /mnt/myswap.swap
 swapon /mnt/myswap.swap
 nano /etc/fstab
-
 /mnt/myswap.swap none swap sw 0 0
 
 wget https://www.golangtc.com/static/go/1.9.1/go1.9.1.linux-amd64.tar.gz
@@ -14,28 +13,20 @@ wget https://github.com/zhq1/bcdn/blob/master/Libevent-release-2.1.8-stable.zip
 wget https://github.com/zhq1/bcdn/blob/master/protobuf-master.zip
 
 sudo apt-get install autoconf automake libtool curl make g++ unzip 
-
 sudo apt-get install libssl-dev 
-unzip Libevent-release-2.1.8-stable
-.ls
-ls
-cd Libevent-release-2.1.8-stable
-./autogen.sh
-chmod -R 755 /Libevent-release-2.1.8-stable
-cd
-chmod -R 755 /Libevent-release-2.1.8-stable
 
+unzip Libevent-release-2.1.8-stable
+chmod -R 755 /Libevent-release-2.1.8-stable
 cd Libevent-release-2.1.8-stable
 ./autogen.sh
 ./configure --prefix=/usr
 make
 sudo make install
 ls -al /usr/lib | grep libevent
-sudo apt-get install autoconf automake libtool curl make g++ unzip 
+
+
 unzip protobuf-master.zip
-
 chmod -R 755 protobuf-master
-
 cd protobuf-master
 ./autogen.sh
 ./configure
@@ -45,7 +36,7 @@ sudo make install
 sudo ldconfig
 protoc --version
 
-cd
+cd /root
 sudo tar -C /usr/local -zxvf go1.9.1.linux-amd64.tar.gz
 sudo mkdir -p ~/go/src
 echo "export GOPATH=$HOME/go" >> ~/.bashrc
